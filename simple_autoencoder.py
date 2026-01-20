f17b28beb6d6184766a8b785c172e1308f40fa4f
fluxtheorist <fluxtheorist@gmail.com>
2026-01-20 12:51:47 -0600

    added decoder

--- simple_autoencoder.py
+++ simple_autoencoder.py
@@ -22,8 +22,50 @@ after_conv3 = conv3(after_conv2)
 # Pass fourth layer
 after_conv4 = conv4(after_conv3)
 
-print(f"Input shape: {fake_image.shape}")
-print(f"After conv1: {after_conv1.shape}")
-print(f"After conv2: {after_conv2.shape}")
-print(f"After conv3: {after_conv3.shape}")
-print(f"After conv4: {after_conv4.shape}")
+# Flatten the conv output
+flattened = after_conv4.view(after_conv4.size(0), -1)
+print(f"Flattened shape: {flattened.shape}")
+
+# Linear layer to get to the latent dimensions
+fc = nn.Linear(in_features=256 * 4 * 4, out_features=32)
+z = fc(flattened)
+print(f"Latent z shape: {z.shape}")
+
+# Linear layer to expand z back to 4096
+fc_decode = nn.Linear(in_features=32, out_features=256 * 4 * 4)
+z_expanded = fc_decode(z)
+print(f"Expanded z shape:{z_expanded.shape}")
+
+# Unflatten back to original
+unflattened = z_expanded.view(z_expanded.size(0), 256, 4, 4)
+print(f"Unflattened shape:{unflattened.shape}")
+
+# Decoder
+transpose1 = nn.ConvTranspose2d(
+    in_channels=256, out_channels=128, kernel_size=4, stride=2, padding=1
+)
+transpose2 = nn.ConvTranspose2d(
+    in_channels=128, out_channels=64, kernel_size=4, stride=2, padding=1
+)
+transpose3 = nn.ConvTranspose2d(
+    in_channels=64, out_channels=32, kernel_size=4, stride=2, padding=1
+)
+transpose4 = nn.ConvTranspose2d(
+    in_channels=32, out_channels=3, kernel_size=4, stride=2, padding=1
+)
+
+# Pass first layer
+after_transpose1 = transpose1(unflattened)
+print(f"After transpose 1:{after_transpose1.shape}")
+
+# Pass second layer
+after_transpose2 = transpose2(after_transpose1)
+print(f"After transpose 2:{after_transpose2.shape}")
+
+# Pass third slayer
+after_transpose3 = transpose3(after_transpose2)
+print(f"After transpose 3:{after_transpose3.shape}")
+
+# Pass fourth layer
+after_transpose4 = transpose4(after_transpose3)
+print(f"After transpose 4:{after_transpose4.shape}")
